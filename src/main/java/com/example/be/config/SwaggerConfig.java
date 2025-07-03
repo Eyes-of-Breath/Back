5ad132a5df9d2b5e7e46245ab6216a33e06ba14e
xaxvun <fbekgus413@naver.com>
2025-07-04 00:54:12 +0900

    환경 설정 및 swagger

---
+++ src/main/java/com/example/be/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.be.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Chest X-ray Diagnosis API",
+                version = "v1.0",
+                description = "AI 기반 폐 질환 진단 서비스 API 문서"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
