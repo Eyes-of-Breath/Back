5c9dcd2766facab74c3a6e4bee17f3a201af226a
xaxvun <fbekgus413@naver.com>
2025-09-21 16:16:53 +0900

    달력 일정 과거 시간 입력 허용

--- src/main/java/com/example/be/dto/request/CreateScheduleEventRequestDto.java
+++ src/main/java/com/example/be/dto/request/CreateScheduleEventRequestDto.java
@@ -1,6 +1,6 @@
 package com.example.be.dto.request;
 
-import jakarta.validation.constraints.FutureOrPresent;
+//import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -13,11 +13,11 @@ public class CreateScheduleEventRequestDto {
     private String title;
 
     @NotNull(message = "시작 시간을 입력해주세요.")
-    @FutureOrPresent(message = "시작 시간은 현재 또는 미래여야 합니다.")
+//    @FutureOrPresent(message = "시작 시간은 현재 또는 미래여야 합니다.")
     private LocalDateTime startTime;
 
     @NotNull(message = "종료 시간을 입력해주세요.")
-    @FutureOrPresent(message = "종료 시간은 현재 또는 미래여야 합니다.")
+//    @FutureOrPresent(message = "종료 시간은 현재 또는 미래여야 합니다.")
     private LocalDateTime endTime;
 
     private String memo;
